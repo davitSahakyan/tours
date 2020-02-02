7c21712cd66ad0fb04af3b78a2fc34f9678636d5
Davit Sahakyan <sahakyandavit92@gmail.com>
2020-02-02 19:40:08 +0400

    docs : added files for app

---
+++ src/data/Data.js
@@ -0,0 +1,34 @@
+export const tourData = [
+  {
+    id: 1,
+    city: "florence",
+    img: "./img/florence.jpg",
+    name: "florence tour",
+    info:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!"
+  },
+  {
+    id: 2,
+    city: "paris",
+    img: "./img/paris.jpg",
+    name: "paris lights tour",
+    info:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!"
+  },
+  {
+    id: 3,
+    city: "london",
+    img: "./img/london.jpg",
+    name: "london royal palace tour",
+    info:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!"
+  },
+  {
+    id: 4,
+    city: "rome",
+    img: "./img/rome.jpg",
+    name: "rome colizey tour",
+    info:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!"
+  }
+];
