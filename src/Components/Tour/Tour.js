d2db62ecd43b1725693fe76519da9efddad232cc
Davit Sahakyan <sahakyandavit92@gmail.com>
2020-02-05 15:34:30 +0400

    feat : add showInfo button

--- src/Components/Tour/Tour.js
+++ src/Components/Tour/Tour.js
@@ -2,8 +2,22 @@ import React, { Component } from 'react';
 import './Tour.scss';
 
 export default class Tour extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      showInfo : false,
+    }
+  }
+
+  handleInfo = () =>{
+    this.setState({
+      showInfo : !this.state.showInfo,
+    })
+  }
+
   render() {
-    const {city , img , name , info} = this.props.tour
+    const {city , img , name , info } = this.props.tour;
+    const { showInfo } = this.state;
     return (
       <article className='tour'>
         <div className='image-container'>
@@ -17,14 +31,12 @@ export default class Tour extends Component {
         <div className='tour-info'>
           <h3>{city}</h3>
           <h4>{name}</h4>
-          <h5>info
+          <h5 onClick={this.handleInfo}>info
             <span>
               <i className='fas fa-caret-square-down' />
             </span>
           </h5>
-          <p>
-            {info}
-          </p>
+          {showInfo &&  <p>{info}</p> }  
         </div>
       </article>
     );
