fc3b58a8a6319afeeecfedf1fade0361b170ebea
Davit Sahakyan <sahakyandavit92@gmail.com>
2020-02-05 17:31:16 +0400

    Updates

---
+++ precache-manifest.139bf22c8f01594e8c73e76c75f05c8d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fca69c5ead8f61d65b0e98b9f87b056",
+    "url": "/tours/index.html"
+  },
+  {
+    "revision": "570aecc49422b7f3a635",
+    "url": "/tours/static/css/main.217a26e3.chunk.css"
+  },
+  {
+    "revision": "039802cab24e8c04078e",
+    "url": "/tours/static/js/2.4d0bb7a0.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/tours/static/js/2.4d0bb7a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "570aecc49422b7f3a635",
+    "url": "/tours/static/js/main.31937cb8.chunk.js"
+  },
+  {
+    "revision": "efc2ef930408a852d78f",
+    "url": "/tours/static/js/runtime-main.cee9950a.js"
+  },
+  {
+    "revision": "fa2322bec8e1d2613e0188a463d5bb0b",
+    "url": "/tours/static/media/logo.fa2322be.png"
+  }
+]);
